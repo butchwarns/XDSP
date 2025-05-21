fff1cd6242094755eeb077a8da4f06657ea46dd7
Butch Warns <contact@butchwarns.de>
2025-05-21 09:57:51 +0200

    Parametrize cv test over frequency at zero volts

--- tests/src/test_cv.cc
+++ tests/src/test_cv.cc
@@ -3,59 +3,78 @@
 
 using namespace xdsp;
 
-#define FREQ_ZERO_VOLT 110.0
-
-void assert_values_correct(double freq, double volt, double zero_volt_freq) {
-  EXPECT_FLOAT_EQ(freq, zero_volt_freq * pow(2.0, volt));
-  EXPECT_FLOAT_EQ(volt, log2(freq / zero_volt_freq));
+void assert_values_correct(double freq, double volt, double freq_zero_volt) {
+  EXPECT_FLOAT_EQ(freq, freq_zero_volt * pow(2.0, volt));
+  EXPECT_FLOAT_EQ(volt, log2(freq / freq_zero_volt));
 }
 
-TEST(CV, VoltPerOct_conversions_static) {
+class CVTest : public ::testing::TestWithParam<double> {
+ protected:
+  void SetUp() override { freq_zero_volt_ = GetParam(); }
+  double freq_zero_volt_;
+};
+
+TEST_P(CVTest, VoltPerOct_conversions_static) {
   for (int i = 0; i < 5; ++i) {
     // Reference values (one octave apart)
     const auto volt0 = (double)i;
     const auto volt1 = (double)i + 1.0;
-    const auto freq0 = FREQ_ZERO_VOLT * pow(2.0, (double)i);
+    const auto freq0 = freq_zero_volt_ * pow(2.0, (double)i);
     const auto freq1 = freq0 * 2;
 
     // Static conversion Volt to Hz
     double freq0_cv =
-        xdsp::cv::VoltPerOct<double>::to_freq(volt0, FREQ_ZERO_VOLT);
+        xdsp::cv::VoltPerOct<double>::to_freq(volt0, freq_zero_volt_);
     double freq1_cv =
-        xdsp::cv::VoltPerOct<double>::to_freq(volt1, FREQ_ZERO_VOLT);
-    assert_values_correct(freq0_cv, volt0, FREQ_ZERO_VOLT);
-    assert_values_correct(freq1_cv, volt1, FREQ_ZERO_VOLT);
+        xdsp::cv::VoltPerOct<double>::to_freq(volt1, freq_zero_volt_);
+    assert_values_correct(freq0_cv, volt0, freq_zero_volt_);
+    assert_values_correct(freq1_cv, volt1, freq_zero_volt_);
 
     // Static conversion Hz to Volt
     double volt0_cv =
-        xdsp::cv::VoltPerOct<double>::to_volt(freq0, FREQ_ZERO_VOLT);
+        xdsp::cv::VoltPerOct<double>::to_volt(freq0, freq_zero_volt_);
     double volt1_cv =
-        xdsp::cv::VoltPerOct<double>::to_volt(freq1, FREQ_ZERO_VOLT);
-    assert_values_correct(freq0, volt0_cv, FREQ_ZERO_VOLT);
-    assert_values_correct(freq1, volt1_cv, FREQ_ZERO_VOLT);
+        xdsp::cv::VoltPerOct<double>::to_volt(freq1, freq_zero_volt_);
+    assert_values_correct(freq0, volt0_cv, freq_zero_volt_);
+    assert_values_correct(freq1, volt1_cv, freq_zero_volt_);
   }
 }
 
-TEST(CV, VoltPerOct_conversions) {
-  auto volt_per_oct = xdsp::cv::VoltPerOct<double>(FREQ_ZERO_VOLT);
+TEST_P(CVTest, VoltPerOct_conversions) {
+  auto volt_per_oct = xdsp::cv::VoltPerOct<double>(freq_zero_volt_);
 
   for (int i = 0; i < 5; ++i) {
     // Reference values (one octave apart)
     const auto volt0 = (double)i;
     const auto volt1 = (double)i + 1.0;
-    const auto freq0 = FREQ_ZERO_VOLT * pow(2.0, (double)i);
+    const auto freq0 = freq_zero_volt_ * pow(2.0, (double)i);
     const auto freq1 = freq0 * 2;
 
     // Conversion Volt to Hz
-    double freq0_cv = volt_per_oct.to_freq(volt0, FREQ_ZERO_VOLT);
-    double freq1_cv = volt_per_oct.to_freq(volt1, FREQ_ZERO_VOLT);
-    assert_values_correct(freq0_cv, volt0, FREQ_ZERO_VOLT);
-    assert_values_correct(freq1_cv, volt1, FREQ_ZERO_VOLT);
+    double freq0_cv = volt_per_oct.to_freq(volt0, freq_zero_volt_);
+    double freq1_cv = volt_per_oct.to_freq(volt1, freq_zero_volt_);
+    assert_values_correct(freq0_cv, volt0, freq_zero_volt_);
+    assert_values_correct(freq1_cv, volt1, freq_zero_volt_);
 
     // Conversion Hz to Volt
-    double volt0_cv = volt_per_oct.to_volt(freq0, FREQ_ZERO_VOLT);
-    double volt1_cv = volt_per_oct.to_volt(freq1, FREQ_ZERO_VOLT);
-    assert_values_correct(freq0, volt0_cv, FREQ_ZERO_VOLT);
-    assert_values_correct(freq1, volt1_cv, FREQ_ZERO_VOLT);
+    double volt0_cv = volt_per_oct.to_volt(freq0, freq_zero_volt_);
+    double volt1_cv = volt_per_oct.to_volt(freq1, freq_zero_volt_);
+    assert_values_correct(freq0, volt0_cv, freq_zero_volt_);
+    assert_values_correct(freq1, volt1_cv, freq_zero_volt_);
   }
 }
+
+std::vector<double> get_freqs_zero_volt_() {
+  std::vector<double> freqs_zero_volt_;
+  for (int i = 0; i < 5; i += 6) {
+    const auto freq_ =
+        0.2 + 2.0 * (double)i;  // Arbitrary values in a sensible range
+    freqs_zero_volt_.push_back(freq_);
+  }
+
+  return freqs_zero_volt_;
+}
+std::vector<double> freqs_zero_volt_ = get_freqs_zero_volt_();
+
+INSTANTIATE_TEST_SUITE_P(CVTestInstance, CVTest,
+                         ::testing::ValuesIn(freqs_zero_volt_));
